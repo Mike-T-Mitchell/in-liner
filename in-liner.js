0dcc736663c959b2b44439d8f0d64c4fb97f1ba9
Mike-T-Mitchell <michael.t.mitchell@outlook.com>
2014-04-07 15:16:31 -0400

    Finished initial version

--- in-liner.js
+++ in-liner.js
@@ -5,7 +5,7 @@ $(function () {
    	header and split on } to read all style declarations 
    	
    	selectorStyles = [
-   		{
+        {
    			'p.1': [
    				{'color': 'green'},
    				{'background': 'gold'}
@@ -25,7 +25,7 @@ $(function () {
 			obj = {};
 	   	// parse out selector; first element in our new array
 		if (cssPair.length === 2) {
-   			cssSelector = $.trim(cssPair[0]);	
+   			cssSelector = $.trim(cssPair[0]);
    			// store selector as key and individual declarations as your values
 	   		obj[cssSelector] = [];
 	   		// split declarations	
@@ -45,18 +45,32 @@ $(function () {
 	   					// assign key/value pairs
 	   					props[splits[0]] = splits[1];
 	   					// pass it to parent object literal
-	   					obj[cssSelector].push(props);	
+	   					obj[cssSelector].push(props);
 	   				}	
 	   			}
 	   		}); 
-	   		// populate parent object literal	
+	   		// populate parent object literal
 	   		selectorStyles.push(obj);
 	   	}
 	});
-
-   	// loop through selectorStyles and use JS to inline the styles on your content
+    // loop through selectorStyles and use JS to inline the styles on your content
 	if (selectorStyles.length > 0) {
-		// to be continued ...
+        for (var i=0; i<selectorStyles.length; i+=1) {
+            var cur = selectorStyles[i],
+                selector = '';
+            for (var k in cur) {
+                var nextObj = cur[k];
+                // css selector
+                selector = k;
+                for (var k2 in nextObj) {
+                    for (var k3 in nextObj[k2]) {
+                        // style key + value pair to be applied
+                        $(selector).css(k3, nextObj[k2][k3]);
+                    }
+                }
+            }
+        }
+        // remove style tags from dom
+        $('style').remove();
 	}
-   console.log(selectorStyles);
 });
