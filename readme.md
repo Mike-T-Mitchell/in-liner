76d9dcdb49d0dea8e2ed474daab222b0390d9050
Mitch <michael.t.mitchell@outlook.com>
2016-04-29 17:19:44 -0400

    updated this to essentially be the same as the FP public-facing version, with a reskin

---
+++ readme.md
@@ -0,0 +1,27 @@
+# Email In-Liner
+### Contributors
+JS: @CLohr and @Mike-T-Mitchell
+HTML/CSS: @Mike-T-Mitchell
+ 
+ 
+### What is this?
+An easy-to use CSS in-liner for e-mail.
+ 
+### Why did you create it?
+Part of my job requires that I code several e-mails, each of which is sent to 100k+ customers, every day. With that kind of volume, these e-mails have to look as _perfect_ as possible across _all_ platforms. The problem is that the most popular e-mail providers (GMail and Outlook included) use pre-processors which remove the `<style>` tags from the markup, as well as any references to external stylesheets (i.e. `<link rel="stylesheet" href="styles/styles.css" />`). As I'm sure you know, an e-mail without styling usually looks... _not great_. Unfortunately, the only way to avoid this is to include _all_ of your styles **inline** (i.e. `<table align="center" style="border-collapse: collapse;" bgcolor="#FFFFFF" border="0" cellpadding="0" cellspacing="0" width="640">`). If you're here reading this right now, you're probably already aware that this gets old _pretty quickly_. Therefore, a co-worker and I decided to create a solution that would let us write e-mail markup the same way we would for any basic web page using a `<style>` element in the head and including all of our selectors/declarations within. Then, all we'd have to do is feed that semantically correct -- but _very_ e-mail **un-ready** -- markup into our solution, and out would come our updated, "in-lined" markup! **This is that.**
+ 
+### How does it modify my code?
+The program is simple --
+ 
+* It begins by grabbing all of your CSS selectors and declarations from within any `<style>` tags it finds and stores them in an object.
+* It then separates those selectors and declarations into _key:value_ pairs and begins comparing the HTML elements in your markup to any selector names it stored in that object.
+* If an element type in your markup matches a selector name, a `style` attribute is added to that element along with the _value_ portion of that _key:value_ pair mentioned previously (i.e. `<table style="border-collapse: collapse;">`)
+ 
+### How do I use it?
+* Clone this repository to your local machine. 
+* Open `index.html` in your browser.
+* Paste your e-mail markup (with the `<style>` tags) into the text field at the top of the page and click "Make Magic."
+* Click the "Copy HTML" button to copy the newly "in-lined" markup (which is displayed in the text field at the bottom of the page) to your clipboard.
+* Profit.
+ 
+Preview: [http://mike-t-mitchell.github.io/in-liner](http://freepeople.github.io/in-liner)
--- scripts/in-liner.js
+++ scripts/in-liner.js
@@ -1,29 +1,18 @@
-/*global console, jQuery*/
+/*
+    TITLE:   HTML In-Liner for E-mail
+    AUTHORS: http://www.github.com/Mike-T-Mitchell and http://www.github.com/CLohr
+ */
 (function ($) {
-    /*
-        Title: HTML In-Liner for E-mail
-        Purpose: read all styles within the <style> tags and directly inline styles via JS
-        Model:
-            selectorStyles = [
-                {
-                    'p.1': [
-                        {'color': 'green'},
-                        {'background': 'gold'}
-                    ]
-                },
-                {
-                    ...
-                }
-            ];
-     */
     'use strict';
     var Inliner = (function () {
+
         var styles = '',
             selectorStyles = [],
             selector = '',
             j;
 
         function traverse(o) {
+
             for (var i in o) {
                 if (o.hasOwnProperty(i)) {
                     if (o[i] !== null && (!$.isNumeric(i)) && typeof o[i] === 'object') {
@@ -35,7 +24,7 @@
                         $(selector).css(i, o[i]);
                     }
                     if (o[i] !== null && typeof o[i] === 'object') {
-                        //going on step down in the object tree
+                        // move along the object tree
                         traverse(o[i]);
                     }
                 }
@@ -43,51 +32,64 @@
         }
 
         function applyStyles() {
+
             // loop through selectorStyles and use JS to inline the styles on your content
             if (selectorStyles.length > 0) {
                 for (j = 0; j < selectorStyles.length; j += 1) {
                     traverse(selectorStyles[j]);
                 }
+
                 // remove style tags from dom
                 $('style').remove();
             }
         }
 
         function createModel() {
+
             // loop through each declaration
             styles = $('#target').find('style').text().split("}");
             $.each(styles, function (index, elementType) {
-                // split on { which will now separate your style selector from declarations
+
+                // split on '{' which will now separate your style selector from declarations
                 var cssPair = $.trim(elementType).split("{"),
                     cssSelector = '',
                     cssDeclarations = '',
                     obj = {};
+
                 // parse out selector; first element in our new array
                 if (cssPair.length === 2) {
                     cssSelector = $.trim(cssPair[0]);
+
                     // store selector as key and individual declarations as your values
                     obj[cssSelector] = [];
+
                     // split declarations
                     cssDeclarations = $.trim(cssPair[1]).split(";");
+
                     // loop through declarations
                     $.each(cssDeclarations, function (i, dec) {
                         var props = {},
                             splits = [];
                         dec = $.trim(dec);
                         if (dec.length > 0) {
+
                             // split this into key/value or style/declaration pairs
                             splits = dec.split(':');
                             if (splits.length === 2) {
+
                                 // trim whitespace
                                 splits[0] = $.trim(splits[0]);
                                 splits[1] = $.trim(splits[1]);
+
                                 // assign key/value pairs
                                 props[splits[0]] = splits[1];
+
                                 // pass it to parent object literal
                                 obj[cssSelector].push(props);
                             }
                         }
                     });
+
                     // populate parent object literal
                     selectorStyles.push(obj);
                 }
@@ -99,35 +101,37 @@
 
         return {
             init: function() {
-                $('.fpSubmitButton').on('click', function() {
-                    // $('.overlay').fadeIn();
+                $('.submit_button').on('click', function() {
 
                     var originalCode = $.trim($('#original').val());
-                    $('#target').html(originalCode);
+                    // $('#target').html(originalCode);
 
+                    // build "busy" spinner model
                     createModel();
 
-                    var parsed = $.trim($('#target').html());
+                    var parsed = $.trim($('#target').html(originalCode));
                     $('#inlined').val(parsed);
 
                     $('#copy_button').clipboard({
                         path: 'jquery.clipboard.swf',
                         copy: function() {
                             $('#copy_button').on('click', function() {
+
                                 // change button text to read 'copied.'
                                 $('#copy_button').val('Copied.');
-                                //change the button text back after 3 seconds
+                                alert('copied');
+
+                                // change the button text back after 3 seconds
                                 setTimeout(function(){
                                     $('#copy_button').val('Copy HTML.');
                                 }, 3000);
-                                //alert("In-lined HTML copied to clipboard.");
+
+                                // alert("In-lined HTML copied to clipboard.");
                             });
+
                             return $('#inlined').val();
                         }
-
                     });
-
-                    // $('.overlay').fadeOut();
                 });
 
                 // clear both fields
@@ -139,6 +143,7 @@
         };
     }());
 
+    // init
     $(function () {
         Inliner.init();
     });
